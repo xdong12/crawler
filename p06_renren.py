40b3378d8c5f3f277cfb69a2e3ef46c360db29ea
xdong12 <xdong12@163.com>
2019-04-08 11:17:39 +0800

    豆瓣电影，韩剧

--- p06_renren.py
+++ p06_renren.py
@@ -3,3 +3,74 @@
 # @Author: Xie1
 # @Time: 19-4-7 19:12
 # @File: p06_renren.py
+
+
+import requests
+import js2py
+
+# 1. 获取requests中的session对象
+session = requests.session()
+# 登录手机端人人网
+# 请求头
+session.headers = {
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mo'
+}
+
+# 2. 使用session发送rKey获取登录需要信息
+# 2.1 准备rkey请求的URL:
+rkey_url = 'http://activity.renren.com/livecell/rKey'
+# 2.2 使用session对象, 发送请求, 获取响应数据
+response = session.get(rkey_url)
+# 2.3 解析响应数据, 获取需要的数据
+# 获取data中数据, 也就是js中n的值; `rkey`也在其中
+n = response.json()['data']
+print(n)
+
+# 3. 使用js2py执行加密密码的js, 获取加密后的密码(难点)
+# 3.1 定义生成加密密码的js 字符串
+js = '''
+     t.password = t.password.split("").reverse().join(""),
+     setMaxDigits(130);
+     var o = new RSAKeyPair(n.e,"",n.n)
+     , r = encryptedString(o, t.password);
+ '''
+
+# 3.2 使用js2py获取js的执行环境
+context = js2py.EvalJs()
+
+# 3.3 使用js的执行环境加载或执行, js所依赖的js文件
+context.execute(session.get(
+    'http://s.xnimg.cn/a85738/wap/mobile/wechatLive/js/BigInt.js').content.decode())
+context.execute(session.get(
+    'http://s.xnimg.cn/a85738/wap/mobile/wechatLive/js/RSA.js').content.decode())
+context.execute(session.get(
+    'http://s.xnimg.cn/a85738/wap/mobile/wechatLive/js/Barrett.js').content.decode())
+
+# 3.4 向js执行环境中,添加js中需要的数据
+# 设置数据n
+context.n = n
+# 设置t.password
+context.t = {
+    'password': 'a123456'  # 准备要加密密码
+}
+# 3.5 执行js生成加密后的密码
+context.execute(js)
+# 3.6 获取加密后的密码
+password = context.r
+print(password)
+
+# 4.  使用session对象发送登录请求, 登录人人网手机端.
+# 4.1 准备登录请求的URL
+login_url = 'http://activity.renren.com/livecell/ajax/clog'
+# 4.2 准备登录请求的数据
+data = {
+    'phoneNum': '15565280933',  # 用户录入, 先写死
+    'password': password,  # js对密码进行加密后的字符串
+    'c1': '-100',  # 固定值, 直接写死
+    'rKey': n['rkey'],  # rkey请求返回的值
+}
+# 4.3 使用session发送登录请求
+response = session.post(login_url, data=data)
+
+# 4.4 打印登录结果
+print(response.content.decode())
---
+++ p08_douban_tv.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Xie1
+# @Time: 19-4-8 11:10
+# @File: p08_douban_tv.py
+
+url = """
+https://m.douban.com/rexxar/api/v2/subject_collection/tv_korean/items?os=ios&for_mobile=1&callback=jsonp1&start=0&count=18&loc_id=108288&_=0
+"""
+
+
+import requests
+import json
+
+
+class DoubanTV(object):
+
+    def __init__(self):
+        #  2.1. start使用占位符进行占位; start={}
+        # 准备URL
+        self.url = 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_korean/items?os=ios&for_mobile=1&&start={}&count=18&loc_id=108288&_=0'
+
+        # 准备请求头
+        self. headers = {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
+            'Referer': 'https://m.douban.com/tv/korean'
+        }
+
+    def get_content_from_url(self, url):
+        """发送请求, 获取响应"""
+        response = requests.get(url, headers=self.headers)
+        # 返回响应内容
+        return response.content.decode()
+
+    def get_data_from_content(self, content):
+        """根据响应内容, 提取电影信息"""
+        # json => python
+        rs = json.loads(content)
+        data = rs['subject_collection_items']
+        return data
+
+    def save_data(self, data):
+        """保存数据"""
+        # 以每一行是一个json格式的数据来写, 这种文件类型为jsonlines
+        with open('json/p08_.jsonlines', 'a', encoding='utf8') as f:
+            for movie in data:
+                json.dump(movie, f, ensure_ascii=False)
+                f.write('\n')
+
+    def run(self):
+
+        # 2.2. 定义start的变量, 用于记录偏移量, 初始值为0
+        start = 0
+        # 2.3. 使用循环不断的发送请求, 每次start递增18
+        while True:
+
+            # 1. 准备URL和请求头
+            url = self.url.format(start)
+            # 2. 发送请求, 获取响应数据
+            content = self.get_content_from_url(url)
+            # 3. 提取电影信息
+            data = self.get_data_from_content(content)
+            # 4. 保存电影信息(以jsonlines, 写入到文件中)
+            self.save_data(data)
+
+            # 2.4. 如果响应电影条数不足18条件了, 结束循环
+            if len(data) < 18:
+                break
+            # 2.3.2 每次start递增18
+            start += 18
+
+
+
+
+if __name__ == '__main__':
+    dbm = DoubanTV()
+    dbm.run()
